f4e66f6311540ce355e18fbd5c4f07ca8e362eb3
lapatatedouce59 <momo29004@gmail.com>
2023-02-24 10:53:48 +0100

    UI fonctionelle + ordres
    - Ajout d'une UI de développement avec tout les voyants
    - Communication avec le serveur (->identification <-data serveur)
    - Commencement ordres client->serveur (->ordre <-Nouvelle data (+refresh auto))

--- ws.js
+++ ws.js
@@ -1,10 +1,13 @@
 const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 8081 });
 const logger = require('./logger')
+const fs = require('fs')
+
+const pccApi=require('./server.json')
 
 const clients = new Map();
 
-wss.on('connection', (ws, req) => {
+wss.on('connection', (ws) => {
     logger.client(true)
 
     ws.on('message', msg => {
@@ -19,7 +22,44 @@ wss.on('connection', (ws, req) => {
         switch(op){
             case 1 :
                 logger.identify(data.ip, data.country, data.city)
+                logger.message('outcome','server.json')
+                //ws.send(JSON.stringify(pccApi));
+                ws.send(JSON.stringify(pccApi));
                 break;
+            case 200 :
+                logger.message('income',JSON.stringify(data))
+                switch(data.execute){
+                    case 'AG':
+                        pccApi.voyGAT=1
+                        pccApi.voyABS=1
+                        pccApi.voyHTV1=1
+                        pccApi.voyHTV2=1
+                        pccApi.voyFSV1=1
+                        pccApi.voyFSV2=1
+                        pccApi.voyHTGAT=1
+                        pccApi.voyFSGAT=1
+                        fs.writeFileSync('./server.json', JSON.stringify(pccApi, null, 2));
+                        ws.send(JSON.stringify({
+                            op: 201,
+                            content: pccApi
+                        }));
+                        break;
+                    case 'AGreset':
+                        pccApi.voyABS=true
+                        pccApi.voyGAT=true
+                        pccApi.voyHTV1=true
+                        pccApi.voyHTV2=true
+                        pccApi.voyFSV1=true
+                        pccApi.voyFSV2=true
+                        pccApi.voyHTGAT=true
+                        pccApi.voyFSGAT=true
+                        fs.writeFileSync('./server.json', JSON.stringify(pccApi, null, 2));
+                        ws.send(JSON.stringify({
+                            op: 201,
+                            content: pccApi
+                        }));
+                        break;
+                }
         }
     })
 
@@ -27,3 +67,4 @@ wss.on('connection', (ws, req) => {
         logger.client(false)
     })
 })
+
